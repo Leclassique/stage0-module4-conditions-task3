29491f7afcdf7ee31fcdd7dd2691b2e39f4c79c3
Leclassique <75534147+Leclassique@users.noreply.github.com>
2023-10-10 20:27:26 +0300

    Completed task

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary <= 10000){
+            double taxes = 0.15;
+            salary = (int) (salary * taxes);
+        } else if (20000>=salary && salary>10000) {
+            double taxes = 0.18;
+            salary = (int) (salary * taxes);
+        } else if (salary>20000) {
+            double taxes = 0.2;
+            salary = (int) (salary * taxes);
+        } else if (salary<0){
+            System.out.println("wrong input!");
+        }
     }
 }
